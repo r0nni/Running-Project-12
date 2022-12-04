0e73b76ec2ce13c0f062a687ef64a1d65c2dd9a5
r0nni <115673678+r0nni@users.noreply.github.com>
2022-12-04 12:25:59 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var height=[30,21,109,78];
+
+console.log(height[2])
+console.log(height)
+for(var i=0;i<height.length;i++){
+  console.log(height[i])
+   }
+  
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
